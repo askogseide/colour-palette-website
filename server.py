965138173520b888c7aab2251fc5506b56d3917f
alexandra.skogseide <alexandra.skogseide@stud.tu-darmstadt.de>
2023-11-20 14:14:44 +0100

    react js frontend and flask python backend

---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask.helpers import send_from_directory
+from flask_cors import CORS, cross_origin
+
+server_app = Flask(__name__, static_folder='frontend/build', static_url_path='')
+cors = CORS(server_app)
+
+@server_app.route('/test', methods=['GET'])
+@cross_origin()
+def index():
+    return{
+        "test": "Flask test"
+    }
+
+@server_app.route('/')
+def serve():
+    return send_from_directory(server_app.static_folder, 'index.html')
+
+if __name__ == '__main__':
+    server_app.run()
